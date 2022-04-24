eb829f9627eec947f498e14a63a9b2584994b4e7
kobepeng <yipeng.info@gmail.com>
2022-04-24 20:36:05 +0800

    style(home): title align with currency area

--- app/web/page/main/view/main.tsx
+++ app/web/page/main/view/main.tsx
@@ -3,7 +3,6 @@ import { Link, Switch } from 'react-router-dom';
 import { hot } from 'react-hot-loader/root'
 import { CRYPTOCURRENCY_LIST } from '../store/constant';
 import Layout from '../../../component/layout'
-import Header from '../../../component/header'
 import Route from '../router/route';
 import Home from '../router/home';
 import './main.css';
@@ -21,7 +20,6 @@ class Main extends Component {
 
   render() {
     return <Layout {...this.props}>
-      <Header></Header>
       <Switch>
         <Route type={CRYPTOCURRENCY_LIST} path="/" component={Home} />
       </Switch>
